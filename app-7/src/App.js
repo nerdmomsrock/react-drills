b5e5d41401dd6a4d9d2f315ce0a4a57ae3eea470
Ryan Morgan <nerdmomsrock@gmail.com>
2021-03-12 16:05:52 -0500

    almost done with app 7

--- app-7/src/App.js
+++ app-7/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todo from './Todo';
+import NewTask from './NewTask';
+import List from './List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+        list: [],
+        input: '',
+        task: [],
+    }
+  }
+
+  handleChange = (val) => {
+    this.setState ({input: val})
+  }
+
+  addItem = (val) => {
+    const input = this.state.input;
+    this.state.list.push(input);
+    this.setState ({input: val});
+  }
+  render () {
+    return (
+      <div className='App'>
+        <h1>My to-do list:</h1>
+        <input placeholder='New Todo Item' onChange={(e) => this.handleChange(e.target.value)} />
+        <button onClick={this.addItem}>Add Item</button>
+        {this.state.list.map((element) => {
+          return <Todo todo={element}/>
+        })}
+        
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ app-7/src/List.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class List extends Component {
+    render () {
+        return (
+            //<h4>{this.props.todo}</h4>
+        )
+    }
+}
+
+export default List
