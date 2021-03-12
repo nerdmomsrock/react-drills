8286bb69a9314fbd31d31e413868cb4b90bde8b0
Ryan Morgan <nerdmomsrock@gmail.com>
2021-03-11 23:40:05 -0500

    app 5 done

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Image from './Image';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  render () {
+    return (
+      <div className='App'>
+        <Image url={'https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png'}/>
+        
+      </div>
+    )
+  }
 }
 
 export default App;
